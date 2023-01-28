ff027ab90599046484c4f1ae6f40dfb167eee132
Sergey <sergey-petric@yandex.ru>
2023-01-28 14:15:46 +0300

    add tuping

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_handlers(callback) -> list:
+    handlers: list = []
+    for step in range(5):
+        # добавляем обработчики для каждого шага (от 0 до 4)
+        handlers.append(lambda: callback(step))
+    return handlers
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def execute_handlers(handlers: list) -> None:
+    # запускаем добавленные обработчики (шаги от 0 до 4)
+    for handler in handlers:
+        handler()
