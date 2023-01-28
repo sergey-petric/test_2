1ab41ba358cd8a856c6510af8a18877f7afb4932
Sergey <sergey-petric@yandex.ru>
2023-01-28 14:16:33 +0300

    fixed a bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def create_handlers(callback) -> list:
     handlers: list = []
     for step in range(5):
         # добавляем обработчики для каждого шага (от 0 до 4)
-        handlers.append(lambda: callback(step))
+        handlers.append(lambda i=step: callback(step))
     return handlers
 
 
